7e00bec9f8c8499c40da9660b44f8c6db909f595
Syafiq Rizky Fauzi <bussinesyafiq@gmail.com>
2023-06-23 07:22:09 +0700

    add:ifle form

---
+++ form.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <form action="post.php" method="post">
+    <label for="fname">First Name</label>
+    <input type="text" name="fname" id="">
+
+    <label for="lname">Last Name</label>
+    <input type="text" name="lname" id="">
+
+    <br>
+    <input type="submit" value="Register">
+  </form>
+</body>
+
+</html>
