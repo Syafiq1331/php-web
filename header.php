df948b4178a9347400ded4efeb8624d0d5e82e21
Syafiq Rizky Fauzi <bussinesyafiq@gmail.com>
2023-06-23 07:22:23 +0700

    add:ifle header

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <style contenteditable style="display: block; white-space:pre;">
+    .header {
+      background-color: #f1f1f1;
+      padding: 20px;
+      text-align: center;
+    }
+  </style>
+</body>
+
+</html>
